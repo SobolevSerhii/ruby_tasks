04876dc82dbb022a9cbe3a9bd264e951565b005c
Sergio9999 <sobolevsergeysom@gmail.com>
2021-01-24 12:51:29 +0200

    add examples to task_2/array.rb in @examples lines

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Array
   def rebuild!
     new_array = []
@@ -5,7 +7,7 @@ class Array
 
     each do |element|
       if element.is_a? Integer
-        new_array << yield(element)
+        new_array <<  yield(element)
       elsif element.is_a? Array
         new_new_array = []
         element.each do |x|
@@ -13,9 +15,22 @@ class Array
         end
         new_array << new_new_array
       elsif element.is_a? String
-        new_array << yield(element.to_s)
+        new_array << yield(element)
       end
     end
     new_array
   end
 end
+
+# # # @examples # # #
+# pry(main)> ['a', 'b', 'c'].rebuild! do |x, size|
+# pry(main)*   size = 5.to_s
+# pry(main)*   x + size
+# pry(main)* end
+# => ["a5", "b5", "c5"]
+
+# pry(main)> [2, [2,5], 7].rebuild! do |x, size|
+# pry(main)*   size = 5
+# pry(main)*   x + size
+# pry(main)* end
+# => [7, [7, 10], 12]
