28dcd03b2330dc990edd98cb1278758703e3d22a
Sergio9999 <sobolevsergeysom@gmail.com>
2021-01-24 10:15:12 +0200

    work by sobolev_s(task 2 not finished full)

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,19 @@
+require 'pry'
 module Truncate
+  def truncate(attribute, length:)
+    instance_variable_set(:@truncate_attribute, attribute.to_s)
+    instance_variable_set(:@truncate_length, length.to_i)
+  end
+
+  def self.extended(klass)
+    class << klass
+      alias_method :__new, :new
+
+      def new(str)
+        truncate_string = str.slice(0..instance_variable_get(:@truncate_length) - 1)
+        e = __new(truncate_string)
+        e
+      end
+    end
+  end
 end
--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,21 @@
 class Array
+  def rebuild!
+    new_array = []
+    return self unless block_given?
+
+    each do |element|
+      if element.is_a? Integer
+        new_array << yield(element)
+      elsif element.is_a? Array
+        new_new_array = []
+        element.each do |x|
+          new_new_array << yield(x)
+        end
+        new_array << new_new_array
+      elsif element.is_a? String
+        new_array << yield(element.to_s)
+      end
+    end
+    new_array
+  end
 end
